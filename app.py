1a9226dba79c9a2d61a1193b15413210f1b54af3
Avi Weinstock <aweinstock314@gmail.com>
2016-08-07 04:20:53 -0400

    Added git routes to the app.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from flask import Flask, request
+from subprocess import Popen, PIPE
 import os
 import sys
 
 app = Flask(__name__)
+
+def premod(s):
+    return "<pre>%s</pre>" % str(s)
 
 def sanitize_html(s):
     s = s.replace("&", "&amp;")
@@ -11,11 +15,11 @@ def sanitize_html(s):
     return s
 
 def formatted_pwd():
-    return "<pre>%s</pre>" % sanitize_html(os.getcwd())
+    return premod(sanitize_html(os.getcwd()))
 
 @app.route("/ls", methods=["GET"])
 def ls():
-    return "<pre>%s</pre>" % sanitize_html("\n".join(sorted(os.listdir('.'))))
+    return premod(sanitize_html("\n".join(sorted(os.listdir(".")))))
 
 @app.route("/pwd", methods=["GET"])
 def pwd():
@@ -23,10 +27,50 @@ def pwd():
 
 @app.route("/cd", methods=["POST"])
 def cd():
-    d = str(request.form.get('dir'))
+    d = str(request.form.get("dir"))
     if os.access(d, os.F_OK):
         sys.stdout.write("chdir: %r\n" % d)
         os.chdir(d)
-    return formatted_pwd()
+        return formatted_pwd()
+    else:
+        return "EACCES"
+
+def zerointeractioncmd(args):
+    proc = Popen(args, stdout=PIPE)
+    (stdout, _) = proc.communicate()
+    return premod(sanitize_html(stdout))
+
+@app.route("/git/diff", methods=["GET"])
+def gitdiff():
+    return zerointeractioncmd(["git", "diff"])
+
+@app.route("/git/log", methods=["GET"])
+def gitlog():
+    return zerointeractioncmd(["git", "log"])
+
+@app.route("/git/status", methods=["GET"])
+def gitstatus():
+    return zerointeractioncmd(["git", "status"])
+
+@app.route("/git/add", methods=["POST"])
+def gitadd():
+    f = str(request.form.get("filename"))
+    if os.access(f, os.F_OK):
+        return zerointeractioncmd(["git", "add", f])
+    else:
+        return "EACCES"
+
+@app.route("/git/commit", methods=["POST"])
+def gitcommit():
+    m = str(request.form.get("message"))
+    return zerointeractioncmd(["git", "commit", "-m", m])
+
+@app.route("/git/pull", methods=["POST"])
+def gitpull():
+    return zerointeractioncmd(["git", "pull"])
+
+@app.route("/git/push", methods=["POST"])
+def gitpush():
+    return zerointeractioncmd(["git", "push"])
 
 app.run(debug=True)
